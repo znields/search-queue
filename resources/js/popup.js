77a01d77c3be631b60a05d1050bd8559758cbbe2
Isaiah Nields <imnields@gmail.com>
2018-04-21 16:34:00 -0400

    Added Comments to Popup.js

--- resources/js/popup.js
+++ resources/js/popup.js
@@ -1,13 +1,19 @@
+/*
+Popup.js contains all functions that are strictly required on the popup.html page.
+*/
+
+// opens the help page on GitHub
 function openHelp() {
     chrome.tabs.create({'url': 'https://github.com/isaiahnields/SearchQueue'});
 }
 
+// creates a Google search for the current search term
 function start() {
     chrome.storage.local.get(null, function (items) {
         search(items["search" + items["index"]], items["prepend"], items["append"]);
     });
 }
-
+// adds event listeners to buttons
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('start').addEventListener('click', start);
     document.getElementById('next').addEventListener('click', next);
