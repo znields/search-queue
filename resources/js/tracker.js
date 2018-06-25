20f1d835669ca780d847fee27742741db80280c1
Isaiah Nields <imnields@gmail.com>
2018-06-25 09:25:15 -0400

    Added Google Analytics

---
+++ resources/js/tracker.js
@@ -0,0 +1,11 @@
+// allows for Google Analytics tracking
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-121318914-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
